d159d566ca3b9b9d66e9dcf1f118d0a1a5385cb1
bharatdevopsinsider-01 <bharat.devopsinsider@gmail.com>
2026-05-26 18:09:30 +0530

    moddi

--- main.tf
+++ main.tf
@@ -11,3 +11,9 @@ resource "azurerm_resource_group" "Bharat" {
     name= ""bharat
     location="eastus" 
 }
+
+resource "azurerm_resource_group" "Bharat" {
+
+    name= ""bharat
+    location="eastus" 
+}
